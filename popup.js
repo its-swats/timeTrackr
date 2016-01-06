3e2194688878d55b88747ebf4b0eb9f44bda8b2c
Shawn Watson <shawnw86@gmail.com>
2016-01-06 08:42:46 -0800

    Convert seconds to HH:MM:SS

--- popup.js
+++ popup.js
@@ -1,30 +1,34 @@
 $(function(){
-  // $('#time').html(chrome.extension.getBackgroundPage().siteTimes[chrome.extension.getBackgroundPage().site])  
   displayTime();
   updateDisplay();
-  
-  // buttonClick();
+  buttonClick();
 })
 
 
+String.prototype.toHHMMSS = function () {
+    var sec_num = parseInt(this, 10); // don't forget the second param
+    var hours   = Math.floor(sec_num / 3600);
+    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    if (seconds < 10) {seconds = "0"+seconds;}
+    var time    = hours+':'+minutes+':'+seconds;
+    return time;
+}
+
 var buttonClick = function(){
+  console.log('test')
   $('#test').on('click', function(event){
-    event.preventDefault();
-    var site = chrome.extension.getBackgroundPage().site
-    $('#time').html(chrome.extension.getBackgroundPage().siteTimes[site])  
-    displayTime();
+    console.log("3489".toHHMMSS());
   })
 }
 
 
 var displayTime = function(){
-  // var site = chrome.extension.getBackgroundPage().site
-  // $('#time').html(chrome.extension.getBackgroundPage().siteTimes[site])  
   setInterval(function(){
-    // $('#time').html(chrome.extension.getBackgroundPage().siteTimes[site])  
     $('#time').html(time())  
-    // console.log(chrome.extension.getBackgroundPage().siteTimes['facebook.com'])
-    // $('#time').html(chrome.extension.getBackgroundPage().siteTimes[chrome.extension.getBackgroundPage().site])  
   }, 1000)
 }
 
@@ -38,5 +42,6 @@ var site = function(){
 }
 
 var time = function(){
-  return chrome.extension.getBackgroundPage().siteTimes[site()]
+  var seconds = chrome.extension.getBackgroundPage().siteTimes[site()].toString();
+  return seconds.toHHMMSS();
 }
