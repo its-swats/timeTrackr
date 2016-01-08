e58d3dfbf696dd86da7cf15cdbf9eb8d6bd50c53
Shawn Watson <shawnw86@gmail.com>
2016-01-08 08:36:45 -0800

    Add ability to delete individual times, and clear all data

--- dashboard.js
+++ dashboard.js
@@ -1,5 +1,8 @@
 $(function(){
   createTable();
+  resetDataHandler();
+  toggleMetrics();
+  deleteLineItem();
 })
 
 var createTable = function(){
@@ -8,19 +11,46 @@ var createTable = function(){
     return parseInt(-array[1])
   })
   _.each(sorted.slice(0,10), function(array){
-    $('#table').append("<tr><td>" + array[0] + "</td><td>" + array[1].toHHMMSS() + "</tr>");
+    $('#table').append("<tr><td><a id='"+array[0]+"' href='#'>&#x2715</a> " + array[0] + "</td><td>" + array[1].toHHMMSS() + "</tr>");
   })
 }
 
 String.prototype.toHHMMSS = function () {
-    var sec_num = parseInt(this, 10); 
-    var hours   = Math.floor(sec_num / 3600);
-    var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-    var seconds = sec_num - (hours * 3600) - (minutes * 60);
+  var sec_num = parseInt(this, 10); 
+  var hours   = Math.floor(sec_num / 3600);
+  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+  var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-    if (hours   < 10) {hours   = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}
-    var time    = hours+':'+minutes+':'+seconds;
-    return time;
+  if (hours   < 10) {hours   = "0"+hours;}
+  if (minutes < 10) {minutes = "0"+minutes;}
+  if (seconds < 10) {seconds = "0"+seconds;}
+  var time    = hours+':'+minutes+':'+seconds;
+  return time;
+}
+
+var resetDataHandler = function(){
+  $('#resetData').on('click', function(event){
+    event.preventDefault();
+    chrome.tabs.getCurrent(function(tab) {
+      localStorage.clear()
+      chrome.tabs.remove(tab.id, function() { 
+      });
+    });
+  })
+}
+var toggleMetrics = function(){
+  $('.toggle-metrics').on('click', function(event){
+    event.preventDefault();
+    $('#metrics').toggleClass('hidden')
+    $('#top10').toggleClass('hidden')
+  })
+}
+
+var deleteLineItem = function(){
+  $('#table').on('click', 'tr td a', function(event){
+    event.preventDefault();
+    var kill = $(this).attr('id');
+    $(this).parent().parent().remove();
+    delete localStorage[kill];
+  })
 }
